d35516e0a8faf5922e0604457b5c3b36a1dbc31f
kanhiya <kanhiyamahajan94@gmail.com>
2022-06-17 12:28:05 +0530

    test

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello1")
+const Honeybadger = require("@honeybadger-io/js");
+
+Honeybadger.configure({
+  apiKey: "hbp_dU4VFZzG1tpmjxNGARqy1NoBDnLOV80JHUtm",
+});
+
+try {
+  Honeybadger.setContext({
+    tags: {
+      severity: "info",
+    },
+  });
+
+  throw "Error from Honeybadger:";
+} catch (e) {
+  Honeybadger.notify(e);
+}
